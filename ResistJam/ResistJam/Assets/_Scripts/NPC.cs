6616663cb48d9293231449fe9f797e49fb08e2f3
Juri Kiin <cornfield24@gmail.com>
2017-03-05 15:23:46 -0500

    - Re-added scene changes in another scene

---
+++ ResistJam/ResistJam/Assets/_Scripts/NPC.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC : MonoBehaviour {
+
+	public MissionType mission;
+
+	public string characterName = "";
+	public string greeting = "";
+	public string[] dialogueOptions;
+	public string[] followUpOptons;
+	public float acceptanceValue = 2.0f;	//%
+	public float movementSpeed = 12.0f;
+	public int[] responseValue;
+	public bool hasCompleted = false;
+	public string missionText;	//Leave blank if no quest (Aside from talking)
+	public GameObject fetchItem;
+	public string endText;
+	public string endOption;
+
+	void Update()
+	{
+		if(hasCompleted)
+		{
+			greeting = endText;
+		}
+	}
+
+
+}
